27b044e7f84d0d126037317ce5af54ea0b02b38b
Unknown <rdleamons@gmail.com>
2019-09-28 16:18:50 -0500

    Auto stash before merge of "master" and "Origin/master"

---
+++ Assets/Scripts/ScreenBoundaries.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBoundaries : MonoBehaviour
+{
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
---
+++ Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textfile;
+    public string[] textlines;
+
+    public int currentLine;
+    public int endLine;
+
+    public CharMover player;
+
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<CharMover>();
+        if (textfile != null)
+        {
+            textlines = (textfile.text.Split('\n'));
+        }
+
+        if(endLine == 0)
+        {
+            endLine = textlines.Length - 1;
+        }
+
+    }
+
+    private void Update()
+    {
+        theText.text = textlines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            currentLine++;
+        }
+
+        if(currentLine > endLine)
+        {
+            textBox.SetActive(false);
+        }
+    }
+
+}
